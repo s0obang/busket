4fecf0c88bb8c537af60be847f675180631bbf89
Son <suruna1026@gmail.com>
2024-10-02 18:10:10 +0900

    유저+프로필 모델작성, 회원가입+로그인 뷰작성

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+
+#password1:비밀번호, password2:비밀번호확인, username:ID
 
 def signup(request):
+    if request.method=='POST':
+        if request.POST['password1']==request.POST['password2']:
+            user=User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'],
+            )
+            auth.login(request, user)
+            return redirect('home:home')
+        return render(request, 'signup.html')
     return render(request, 'signup.html')
 
+
 def login(request):
-    return render(request, 'login.html')
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home:home')
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
 
 def my(request):
     return render(request, 'my.html')
--- busket/urls.py
+++ busket/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home.views import home
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('accounts/', include('accounts.urls')),
     path('buket/', include('buket.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
