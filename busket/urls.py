b96f9c3670ebbae9477cee7827d34cb1af9506b7
sujin <csujin2040@gmail.com>
2024-10-10 13:14:45 +0900

    1차병합

--- busket/urls.py
+++ busket/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
     path('buket/', include('buket.urls')),
 ]
